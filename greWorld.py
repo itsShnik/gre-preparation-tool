7c13cc28ab3c96e134ae884089146a2c96f8ec90
itsShnik <shnik456@iitkgp.ac.in>
2020-04-08 17:40:42 +0530

    added a few functions like search

--- greWorld.py
+++ greWorld.py
@@ -3,12 +3,21 @@ from bs4 import BeautifulSoup
 import json
 import sys
 
-f = open("greWordList.json", 'r')
 try:
+    f = open("greWordList.json", 'r')
     global_dict = json.load(f)
+    f.close()
 except:
+    #print("unable to find json file, creating a new one!")
     global_dict = {}
-f.close()
+
+try:
+    f = open('vocabulary.json', 'r')
+    vocab_dict = json.load(f)
+    f.close()
+except:
+    #print("vocabulary.json does not exist!")
+    vocab_dict = {}
 
 def scrapeAListFromVocabulary(url, list_name, length):
     # get the content of the page
@@ -54,6 +63,11 @@ def scrapeAListFromVocabulary(url, list_name, length):
 
     if len(temp_word_list) > 0:
         global_dict[list_name] = temp_word_list
+        print("list succesfully added!")
+        print("updating the local vocabulary....")
+        updateVocab()
+    else:
+        print("UNSUCCESSFUL!!!")
 
 def displayAllLists():
     for list_name in global_dict.keys():
@@ -63,14 +77,28 @@ def displayAllLists():
         print("-----------------------------------")
         print()
 
+def updateVocab():
+    
+    for key in global_dict.keys():
+        for word_dict in global_dict[key]:
+            if word_dict["word"] not in vocab_dict:
+                vocab_dict[word_dict["word"]] = word_dict["definition"] 
+
+    with open('vocabulary.json', 'w') as f:
+        json.dump(vocab_dict, f)
+        print("Local vocabulary successfully updated")
+        f.close()
+
 def printMenu():
-    print("------------------------------------------------------------------")
-    print("1. Display all the available lists                                ")
-    print("2. Add a list from Vocabulary.com                                 ")
-    print("3. Learn from a list                                              ")
-    print("4. Take a test                                                    ")
-    print("5. Exit                                                           ")
-    print("------------------------------------------------------------------")
+    print("------------------------------------")
+    print("1. Display all the available lists")
+    print("2. Add a list from Vocabulary.com")
+    print("3. Learn from a list")
+    print("4. Take a test")
+    print("5. Update the local Vocabulary")
+    print("6. Search a word")
+    print("7. Exit")
+    print("------------------------------------")
 
 def addAList():
     print("\nEnter the url:")
@@ -85,8 +113,20 @@ def addAList():
         json.dump(global_dict, f)
         f.close()
 
+def searchInVocab():
+    while(True):
+        word_to_search = str(input())
+        if(len(word_to_search) <= 1):
+            break
+        if word_to_search in vocab_dict.keys():
+            print(vocab_dict[word_to_search])
+            print()
+        else:
+            print("word not found\n")
+
 def Learn():
     print("function in build")
+
 
 def test():
     print("function in build")
@@ -111,6 +151,10 @@ def main():
         elif choice == 4:
             test()
         elif choice == 5:
+            updateVocab()
+        elif choice == 6:
+            searchInVocab()
+        elif choice == 7:
             sys.exit()
 
 if __name__=='__main__':
